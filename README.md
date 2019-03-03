7d444bf8ea1832c5721d9d6e195fd6140567571d
Sean OSullivan <sosman646@gmail.com>
2019-03-03 15:00:21 -0500

    Fully added current model ducation, AwardsCertifications and Volunteering. Updated readme to include more project details.
    Signed-off-by: Sean OSullivan <sosman646@gmail.com>

--- README.md
+++ README.md
@@ -5,8 +5,14 @@ Model layout will include several attributes of a portfolio:
     Homepage with a preview view of each of the side pagaes
     Work Experiences
     Projects
-    Basic Bio
+    Basic Bio with Skills Summary
     Eductation
     Awards and Certifications
     Volunteer Experience
-    Skills Summary
+
+Website layout will include a main page for the user to interact with a compact resume like format of the portfolio with a peek at each of the overall models.
+
+The models will each be fully presented in a seperate view with each entry having its own page to be presented on with a default view being developed for each of them.
+
+Certain models will not have their own page such as the Bio which will be remain as a mainpage or even a header, this is currently undecided.
+
--- portfolio_site/portfolio/models.py
+++ portfolio_site/portfolio/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class workExperiences(models.Model):
+class WorkExperiences(models.Model):
     job_title = models.CharField(max_length=200)
     company_name = models.CharField(max_length=200)
     location = models.CharField(max_length=200)
@@ -21,6 +21,45 @@ class Bio(models.Model):
     current_job = models.CharField(max_length=200)
     current_company = models.CharField(max_length=200)
     description = models.TextField()
+    skill_summary = models.TextField()
 
     def __str__(self):
         return self.title
+
+class Projects(models.Model):
+    project_name = models.CharField(max_length=200)
+    team = models.BooleanField()
+    teamates = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.project_name
+
+class Education(models.Model):
+    school_name = models.CharField(max_length=200)
+    qualification = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.school_name
+
+class AwardsCertifications(models.Model):
+    certificate_name = models.CharField(max_length=200)
+    certificate_organization = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.certificate_name
+
+class VolunteerExperience(models.Model):
+    job_title = models.CharField(max_length=200)
+    organization_name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    start_date =  models.DateTimeField('Start Date')
+    end_date = models.DateTimeField('End Date')
+    current = models.BooleanField()
+
+    # add image of the organizations logo eventually
+
+    job_description = models.TextField()
+
+    def __str__(self):
+        return self.job_title
