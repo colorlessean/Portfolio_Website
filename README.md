4596dfc6d4823b9e1aaeac8b8e98943d30e55fcc
Sean OSullivan <sosman646@gmail.com>
2019-03-03 14:37:03 -0500

    Added Bio and Work Experience models and performed migration to include both
    Signed-off-by: Sean OSullivan <sosman646@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Portfolio_Website
 Django built website for personal portfolio matters, with a focus on expand ability.
+
+Model layout will include several attributes of a portfolio:
+    Homepage with a preview view of each of the side pagaes
+    Work Experiences
+    Projects
+    Basic Bio
+    Eductation
+    Awards and Certifications
+    Volunteer Experience
+    Skills Summary
--- portfolio_site/portfolio/models.py
+++ portfolio_site/portfolio/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class workExperiences(models.Model):
+    job_title = models.CharField(max_length=200)
+    company_name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    start_date =  models.DateTimeField('Start Date')
+    end_date = models.DateTimeField('End Date')
+    current_job = models.BooleanField()
+
+    # add image of the companies logo eventually
+
+    job_description = models.TextField()
+
+    def __str__(self):
+        return self.job_title
+
+class Bio(models.Model):
+    title = models.CharField(max_length=200)
+    current_job = models.CharField(max_length=200)
+    current_company = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
